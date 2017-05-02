46babe21efde4da5509c050284164d4655eb8d75
Daniel Maslowski <daniel.maslowski@shop.co>
2017-05-02 09:34:35 +0200

    fix spelling of "listening"

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const micro = require('micro')
 const port = process.env.PORT || 3000
 
 const server = micro(async () => 'Hello world')
-server.listen(port, () => console.log(`Listing on port ${port}`))
+server.listen(port, () => console.log(`Listening on port ${port}`))
